5c6c0c4d1aa7223216193faa0e64d3fbb7aba243
Tiago S.C <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2023-10-14 14:06:28 -0300

    :tada: inital commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//npx tsx src/index.ts ==> converte o arquivo para js sem criar arquivo
+//tsx ==> se usa em desenvolvimento, em produção usa-se tsc
+// npx watch tsx src/index.ts ==> roda o app e fica escutando as alterações
+//npx tsc src/index.ts ==> converte o arquivo para js e cria um arquivo js
+import fastify from "fastify";
+import { knex } from "./database";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const tables = await knex("sqlite_schema").select("*");
+
+  return tables;
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => console.log("Server is running on port 3000"));
